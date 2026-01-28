d648c933c4ab13d53d50c22236bd819074054fed
Shameen Bappy <shameenbappy3@gmail.com>
2026-01-29 03:43:00 +0600

    Create function called playGame
    This function uses loop to run playRound 5 times.After excution it
    consoles player and computer scores and returns a string saying who
    won.

--- script.js
+++ script.js
@@ -15,58 +15,77 @@ function getHumanChoice(){
             return "Paper!";
             break;
         case "SCISSORS":
-            return "Scissors";
+            return "Scissors!";
             break;
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let draw = 0;
 
 function playRound(humanChoice,computerChoice){
- if(humanChoice === "Rock!"){
-    if(computerChoice === "Paper!"){
-        computerScore++;
-        return "You Lose! Paper Beats Rock!"
-    };
-    if(computerChoice === "Scissors!"){
-        humanScore++;
-        return "You Win! Rock Beats Scissors!"
-    };
-    if(computerChoice === "Rock!"){
-        return "Draw! You both chose Rock!"
-    };
- }
- if(humanChoice === "Paper!"){
-    if(computerChoice === "Rock!"){
-        humanScore++;
-        return "You Win! Paper Beats Rock!";
+
+    if(humanChoice === "Rock!"){
+        if(computerChoice === "Paper!"){
+            computerScore++;
+            return "You Lose! Paper Beats Rock!"
+        };
+        if(computerChoice === "Scissors!"){
+            humanScore++;
+            return "You Win! Rock Beats Scissors!"
+        };
+        if(computerChoice === "Rock!"){
+            draw++;
+            return "Draw! You both chose Rock!"
+        };
     }
-    if(computerChoice === "Scissors!"){
-        computerScore++;
-        return "You Lose! Scissors beats Paper!";
+    if(humanChoice === "Paper!"){
+        if(computerChoice === "Rock!"){
+            humanScore++;
+            return "You Win! Paper Beats Rock!";
+        }
+        if(computerChoice === "Scissors!"){
+            computerScore++;
+            return "You Lose! Scissors beats Paper!";
+        }
+        if(computerChoice ==="Paper!"){
+            draw++;
+            return "Draw! You Both Choose Papers!";
+        }
     }
-    if(computerChoice ==="Paper!"){
-        return "Draw! You Both Choose Papers!";
+    if(humanChoice === "Scissors!"){
+        if(computerChoice === "Rock!"){
+            computerScore++;
+            return "You Lose! Rock Beats Scissors!";
+        }
+        if(computerChoice === "Paper!"){
+            humanScore++;
+            return "You Win! Scissors Beats Papers!"
+        }
+        if(computerChoice === "Scissors!"){
+            draw++;
+            return "Draw! You both choose scissors!"
+        }
     }
- }
- if(humanChoice === "Scissors!"){
-    if(computerChoice === "Rock!"){
-        computerScore++;
-        return "You Lose! Rock Beats Scissors!";
-    }
-    if(computerChoice === "Paper!"){
-        humanScore++;
-        return "You Win! Scissors Beats Papers!"
-    }
-    if(computerChoice === "Scissors!"){
-        return "Draw! You both choose scissors!"
-    }
- }
 };
 
-let humanSelection = getHumanChoice();
-let computerSelection = getComputerChoice();
 
-console.log(playRound(humanSelection,computerSelection));
 
+
+function playGame(){
+    for(let i = 0; i < 5 ; i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection,computerSelection);
+    }
+    if(humanScore > computerScore){
+        console.log(`You: ${humanScore} Computer: ${computerScore} Draw: ${draw}`);
+        return console.log("You Won!!!");
+    }else{
+        console.log(`You: ${humanScore} Computer: ${computerScore} Draw: ${draw}`);
+        return console.log("You Lose!")
+    };
+}
+
+playGame();
